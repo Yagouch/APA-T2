2753b45b8b4918e244dda3660dbf1df3e5a5fe24
Yagouch <yagocarballo@estudiantat.upc.edu>
2024-04-12 10:39:34 +0200

    Refactor funtions

--- primos.py
+++ primos.py
@@ -47,88 +47,49 @@ def descompon(numero):
             
     return tuple(lista)
 
-def mcm(numero1, numero2):
+def mcm(a, b):
     """
     Devuelve el mínimo común múltiplo de sus argumentos.
     >>> mcm(90, 14)
     630
     """
-    if numero1 <= 1 or esPrimo(numero1):
-        if numero1 in descompon(numero2):
-            return numero2
+    factores_a = list(descompon(a))
+    factores_b = list(descompon(b))
+
+    mcm = 1
+    i,j = 0, 0
+
+    for i in factores_a:
+        if i in factores_b:
+            mcm *= i
+            factores_b.remove(i)
         else:
-            return numero1*numero2
-        
-    if numero2 <= 1 or esPrimo(numero2):
-        if numero2 in descompon(numero1):
-            return numero1
-        else:
-            return numero1*numero2
-        
-    exponentes1 = {}
-    exponentes2 = {}
-
-    for i in descompon(numero1):
-        if i in exponentes1:
-            exponentes1[i] += 1
-        else:
-            exponentes1[i] = 1
-
-    for i in descompon(numero2):
-        if i in exponentes2:
-            exponentes2[i] += 1
-        else:
-            exponentes2[i] = 1
-
-    tot = 1
-
-    for k in exponentes1:
-        if k in exponentes2:
-            if exponentes1[k] > exponentes2[k]:
-                tot *= k**exponentes1[k]
-            else:
-                tot *= k**exponentes2[k]
-        else:
-            tot *= k**exponentes1[k]
-
-    for k in exponentes2:
-        if k not in exponentes1:
-            tot *= k**exponentes2[k]
-
-    return tot
+            mcm *= i
 
 
-def mcd(numero1, numero2): 
+    for i in factores_b:
+        mcm *= i
+
+    return mcm
+
+
+def mcd(a, b): 
     """
     Devuelve el máximo común divisor de sus argumentos.
     >>> mcd(924, 780)
     12
     """
-    componentes1 = {}
-    componentes2 = {}
+    factores_a = list(descompon(a))
+    factores_b = list(descompon(b))
 
-    for key in descompon(numero1):
-        if key in componentes1:
-            componentes1[key] += 1
-        else:
-            componentes1[key] = 1
+    mcd = 1
 
-    for key in descompon(numero2):
-        if key in componentes2:
-            componentes2[key] += 1
-        else:
-            componentes2[key] = 1
+    for i in factores_a:
+        if i in factores_b:
+            factores_b.remove(i)
+            mcd *= i
     
-    tot = 1
-
-    for key in componentes1:
-        if key in componentes2:
-            if componentes1[key] < componentes2[key]:
-                tot *= key**componentes1[key]
-            else:
-                tot *= key**componentes2[key]
-
-    return tot
+    return mcd
 
 def mcmN(*numeros):
     """
